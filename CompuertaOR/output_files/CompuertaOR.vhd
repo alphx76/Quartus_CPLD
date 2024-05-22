4bc7bca325f139e9afa17325c6f7abf1fe9d5ef2
Dr. José Alfonso D. C. <95945447+alphx76@users.noreply.github.com>
2024-05-22 06:30:18 -0600

    Create CompuertaOR.vhd

---
+++ CompuertaOR/output_files/CompuertaOR.vhd
@@ -0,0 +1,21 @@
+-- DI seno e
+compuerta or
+library IEEE;
+_ LOGIC_1164. ALL ;
+use IEEE.STD
+use IEEE.
+use
+aentity proyectol is
+a port (A
+B
+x
+end entity;
+. in STD_LOGIC;
+. in STD_LOG1q,•
+. out STD_LOGIC);
+aarchitecture compuerta_or of proyectol is
+abegin
+salida<= entradal
+entrada2;
+or
+end architecture;
